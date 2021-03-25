70c2f7eeb632e9c69448b63358709cad27c83ad0
vinodh7899 <vinodhcena.7@gmail.com>
2021-03-25 10:56:50 +0530

    ccc

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,7 +4,6 @@ from django import forms
 from django.core import validators
 
 CITY_CHOICES =( 
-    ("",""),
     ("Bengaluru", "Bengaluru"), 
     ("Chennai", "Chennai"), 
     ("Hyderabad", "Hyderabad"),
@@ -13,7 +12,7 @@ class RegisterForm(forms.Form):
     Name = forms.CharField(widget=forms.TextInput(attrs={'class':"input--style-4",'pattern':'[A-Za-z ]+', 'title':'Enter Characters Only '}))
     username = forms.CharField(widget=forms.TextInput(attrs={'class':'input--style-4'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'input--style-4','pattern':"[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}",'title':"Please Enter Valid Email Id eg:V****@mail.com"}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'input--style-4','pattern':"(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}",'title':'Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'input--style-4','pattern':"(?=.*\d)(?=.*[a-z]).{8,}",'title':'Must contain at least one number and one  letter, and at least 8 or more characters'}))
     confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'input--style-4'}))
     mobile = forms.CharField(widget=forms.TextInput(attrs={'class':'input--style-4','pattern':'[6-9]{4}[0-9]{6}','title':"Please Enter Valid 10 Mobile Number"}))
     default_address = forms.CharField(widget=forms.TextInput(attrs={'class':'input--style-4'}))
